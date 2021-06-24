b2996efacf35302312b271b854339bb2125979ad
Santosh-911 <santosh911@gmail.com>
2021-06-24 23:11:31 +0530

    updated code

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return "<h1>This is flask application, created by Santosh</h1>"
+    return "<h1>This is flask application, created by Santosh and checking again.</h1>"
 
 
 if __name__ == "__main__":
